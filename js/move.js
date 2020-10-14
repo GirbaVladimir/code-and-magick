c1d1c8db0059798c2c36f7fc4af2073a3c83529a
Vladimir <girba.vladimir@yandex.ru>
2020-10-14 16:43:16 +0300

    module5-task1 fixes

--- js/move.js
+++ js/move.js
@@ -1,7 +1,7 @@
 'use strict';
 (function () {
-  const setupDialogElement = document.querySelector('.setup');
-  const dialogHandle = setupDialogElement.querySelector('.upload');
+  const setupDialogElement = document.querySelector(`.setup`);
+  const dialogHandle = setupDialogElement.querySelector(`.upload`);
 
   dialogHandle.addEventListener(`mousedown`, function (evt) {
     evt.preventDefault();
@@ -29,26 +29,26 @@
       };
 
       setupDialogElement.style.top = (setupDialogElement.offsetTop - shift.y) + `px`;
-      setupDialogElement.style.left = (setupDialogElement.offsetLeft - shift.x) + 'px';
+      setupDialogElement.style.left = (setupDialogElement.offsetLeft - shift.x) + `px`;
 
     };
 
     const onMouseUp = function (upEvt) {
       upEvt.preventDefault();
 
-      document.removeEventListener('mousemove', onMouseMove);
-      document.removeEventListener('mouseup', onMouseUp);
+      document.removeEventListener(`mousemove`, onMouseMove);
+      document.removeEventListener(`mouseup`, onMouseUp);
 
       if (dragged) {
         const onClickPreventDefault = function (clickEvt) {
           clickEvt.preventDefault();
-          dialogHandle.removeEventListener('click', onClickPreventDefault);
+          dialogHandle.removeEventListener(`click`, onClickPreventDefault);
         };
-        dialogHandle.addEventListener('click', onClickPreventDefault);
+        dialogHandle.addEventListener(`click`, onClickPreventDefault);
       }
     };
 
-    document.addEventListener('mousemove', onMouseMove);
-    document.addEventListener('mouseup', onMouseUp);
+    document.addEventListener(`mousemove`, onMouseMove);
+    document.addEventListener(`mouseup`, onMouseUp);
   });
 })();
